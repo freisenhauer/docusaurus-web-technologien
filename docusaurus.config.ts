036ec340f2114faf10aeb5f086b9c5a07179cc42
Frederic Reisenhauer <mail@freisenhauer.de>
2025-12-08 16:03:03 +0100

    add new favicon and logo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Web Technologien',
   tagline: 'Skript und Unterlagen zur Vorlesung "Web Technologien" von Frederic Reisenhauer',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
